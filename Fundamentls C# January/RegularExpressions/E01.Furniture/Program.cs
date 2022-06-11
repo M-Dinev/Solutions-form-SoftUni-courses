0c3a62f890aa3ccc210039397fbe863d347b9223
M-Dinev <m.z.dinev@abv.bg>
2022-06-11 09:39:56 +0300

    upload task

---
+++ Fundamentls C# January/RegularExpressions/E01.Furniture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
