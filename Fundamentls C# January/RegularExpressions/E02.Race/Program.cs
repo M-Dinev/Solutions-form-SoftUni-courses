bb3ddc50cc84381225bf1a93ad39f15e1792e0cb
M-Dinev <m.z.dinev@abv.bg>
2022-06-13 16:49:37 +0300

    upload task

---
+++ Fundamentls C# January/RegularExpressions/E02.Race/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E02.Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
