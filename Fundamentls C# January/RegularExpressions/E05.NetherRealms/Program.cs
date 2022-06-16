586f18e6d103a0bb572f617ab7e7c3560403098e
M-Dinev <m.z.dinev@abv.bg>
2022-06-16 08:03:54 +0300

    upload task

---
+++ Fundamentls C# January/RegularExpressions/E05.NetherRealms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E05.NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
