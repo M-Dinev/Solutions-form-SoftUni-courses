a52be1dda552c5edf82382017b691c3b06c81bbb
M-Dinev <m.z.dinev@abv.bg>
2022-06-17 07:55:24 +0300

    upload task

---
+++ Fundamentls C# January/RegularExpressions/E06.ExtractEmails/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
