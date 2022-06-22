521264910f4fc3b24cddb25eb775243837011a95
M-Dinev <m.z.dinev@abv.bg>
2022-06-22 15:53:02 +0300

    upload task

---
+++ Fundamentls C# January/RegularExpressions/FE04.FancyBarcodes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FE04.FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
